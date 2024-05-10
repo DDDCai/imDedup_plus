f446c686609cd4aa233d991e7f7628bec799e9a8
DDDCai <dengcaidengcai@163.com>
2024-05-10 21:25:09 +0800

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -3,7 +3,7 @@
 sudo sync
 sudo echo 3 > /proc/sys/vm/drop_caches
 sudo rm -r /home/dc/data/image_data/dst/test/*
-sudo /home/dc/image_dedup/iDedup/build/sid -c --input_path=/home/dc/data/image_data/test/ \
+sudo /home/dc/image_dedup/imDedup/build/sid -c --input_path=/home/dc/data/image_data/test/ \
         --output_path=/home/dc/data/image_data/dst/test/ --read_thrd_num=1 --decode_thrd_num=8 --middle_thrd_num=4 --rejpeg_thrd_num=8 \
         --write_thrd_num=1 --buffer_size=G64 --patch_size=G1 --name_list=G1 --read_list=G2 --indx_list=G2 \
         --decd_list=G2 --dect_list=G2 --deup_list=G2 --rejg_list=G2 --chunking=variable \
@@ -16,7 +16,7 @@ sudo /home/dc/image_dedup/iDedup/build/sid -c --input_path=/home/dc/data/image_d
 sudo sync
 sudo echo 3 > /proc/sys/vm/drop_caches
 sudo rm -r /home/dc/data/image_data/rst/test/*
-sudo /home/dc/image_dedup/iDedup/build/sid -d --input_path=/home/dc/data/image_data/dst/test/ \
+sudo /home/dc/image_dedup/imDedup/build/sid -d --input_path=/home/dc/data/image_data/dst/test/ \
         --output_path=/home/dc/data/image_data/rst/test/ --middle_thrd_num=4 --buffer_size=G64 --read_list=G2 \
         --jpeg_list=G2 --decd_list=G2 --deup_list=G2 --encd_list=G2 \
         --reference_path=/home/dc/data/image_data/test_all/
